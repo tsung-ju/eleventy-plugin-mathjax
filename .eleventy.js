40e414a1b93aeca6da5efe44bc84c534e5d043b8
Tsung-Ju Chiang <71379180+tsung-ju@users.noreply.github.com>
2021-07-21 04:08:08 +0000

    Initial commit

---
+++ .eleventy.js
@@ -0,0 +1,100 @@
+const { mathjax } = require("mathjax-full/js/mathjax.js");
+const { TeX } = require("mathjax-full/js/input/tex.js");
+const { SVG } = require("mathjax-full/js/output/svg.js");
+const { CHTML } = require("mathjax-full/js/output/chtml.js");
+const { liteAdaptor } = require("mathjax-full/js/adaptors/liteAdaptor.js");
+const { RegisterHTMLHandler } = require("mathjax-full/js/handlers/html.js");
+const { AssistiveMmlHandler } = require("mathjax-full/js/a11y/assistive-mml.js");
+
+const { AllPackages } = require("mathjax-full/js/input/tex/AllPackages.js");
+
+const defaultOptions = {
+  output: "svg",
+  tex: {
+    packages: AllPackages,
+    inlineMath: [
+      ["$", "$"],
+      ["\\(", "\\)"],
+    ],
+  },
+  chtml: {},
+  svg: {
+    fontCache: "global",
+  },
+  adaptor: {},
+};
+
+module.exports = function (eleventyConfig, options) {
+  options = {
+    ...defaultOptions,
+    ...options,
+    tex: { ...defaultOptions.tex, ...options.tex },
+    svg: { ...defaultOptions.svg, ...options.svg },
+    chtml: { ...defaultOptions.chtml, ...options.chtml },
+    adaptor: { ...defaultOptions.adaptor, ...options.adaptor },
+  };
+
+  const InputJax = new TeX(options.tex);
+
+  const OutputJax = createOutputJax(options);
+
+  const adaptor = liteAdaptor(options.adaptor);
+  AssistiveMmlHandler(RegisterHTMLHandler(adaptor));
+
+  eleventyConfig.addTransform("mathjax", function (content, outputPath) {
+    if (!(outputPath && outputPath.endsWith(".html"))) {
+      return content;
+    }
+
+    const html = mathjax.document(content, { InputJax, OutputJax });
+    html.render();
+
+    cleanOutput(html, adaptor, options);
+
+    return (
+      adaptor.doctype(html.document) +
+      "\n" +
+      adaptor.outerHTML(adaptor.root(html.document)) +
+      "\n"
+    );
+  });
+};
+
+function createOutputJax(options) {
+  switch (options.output) {
+    case "svg":
+      return new SVG(options.svg);
+    case "chtml":
+      return new CHTML(options.chtml);
+    default:
+      throw new TypeError("Unsupported output format");
+  }
+}
+
+function cleanOutput(html, adaptor, options) {
+  if (isEmpty(html.math)) {
+    switch (options.output) {
+      case "svg": {
+        adaptor.remove(html.outputJax.svgStyles);
+        const globalCache = adaptor.elementById(
+          adaptor.body(html.document),
+          "MJX-SVG-global-cache"
+        );
+        if (globalCache != null) {
+          adaptor.remove(globalCache);
+        }
+        break;
+      }
+      case "chtml":
+        adaptor.remove(html.outputJax.chtmlStyles);
+        break;
+    }
+  }
+}
+
+function isEmpty(iterable) {
+  for (const elem of iterable) {
+    return false;
+  }
+  return true;
+}
---
+++ README.md
@@ -0,0 +1,81 @@
+# eleventy-plugin-mathjax
+
+Eleventy plugin for server-side MathJax rendering.
+
+## Installation
+
+```sh
+npm install eleventy-plugin-mathjax
+```
+
+```js
+const mathjaxPlugin = require("eleventy-plugin-mathjax");
+
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPlugin(mathjaxPlugin);
+};
+```
+
+### Options
+
+Optionally pass in an options object as the second argument to addPlugin to further customize this plugin pack.
+
+For example, to use the CommonHTML output format instead of SVG:
+
+```js
+eleventyConfig.addPlugin(mathjaxPlugin, {
+  output: "chtml",
+  chtml: {
+    fontURL:
+      "https://cdn.jsdelivr.net/npm/mathjax@3/es5/output/chtml/fonts/woff-v2",
+  },
+});
+```
+
+#### output
+
+Output format for math equations. Supports `"svg"` and `"chtml"`. Defaults to `"svg"`.
+
+#### tex
+
+Options for MathJaX's TeX input processor.
+
+Defaults to:
+
+```
+{
+  packages: /* all packages */,
+  inlineMath: [
+    ["$", "$"],
+    ["\\(", "\\)"],
+  ],
+}
+```
+
+See [here](https://docs.mathjax.org/en/latest/options/input/tex.html) for full options.
+
+#### svg
+
+Options for MathJaX's SVG output processor.
+
+Defaults to:
+
+```js
+{
+  fontCache: "global",
+}
+```
+
+See [here](https://docs.mathjax.org/en/latest/options/output/svg.html) for full options.
+
+#### chtml
+
+Options for MathJaX's CommonHTML output processor. Defaults to `{}`.
+
+See [here](https://docs.mathjax.org/en/latest/options/output/chtml.html) for full options.
+
+#### adaptor
+
+Options for MathJaX's lite DOM adaptor. Useful for passing size hints to MathJaX, e.g. `{ fontSize: 18 }`. Defaults to `{}`.
+
+See [here](https://github.com/mathjax/MathJax-src/blob/master/ts/adaptors/liteAdaptor.ts) for full options.
